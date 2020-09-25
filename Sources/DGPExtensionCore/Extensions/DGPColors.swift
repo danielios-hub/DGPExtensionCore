f13117c7ce95baeda43f4b339e9835b4fae9b9d6
Daniel <1asir.daniel.gallego@gmail.com>
2020-09-25 17:16:19 +0200

    - Fix acl for all extensions

--- Sources/DGPExtensionCore/Extensions/DGPColors.swift
+++ Sources/DGPExtensionCore/Extensions/DGPColors.swift
@@ -11,42 +11,9 @@ import UIKit
 
 extension UIColor {
     
-    static let emerald = UIColor(hex: 0x009B77)
-    static let emeraldDark = UIColor(hex: 0x2D7F77)
-    
-    static let lightGray = UIColor(hex: 0xecf0f1)
-    static let silver = UIColor(hex: 0xbdc3c7)
-    
-    static let mimosa = UIColor(hex: 0xEFC050)
-    
-    static let transparentBlack = UIColor(hex: 0x000000, a: 0.5)
-    static let black_midnight_light = UIColor(hex: 0x404040)
-    
-    static let red_fluorescent = UIColor(hex: 0xff5252)
-    static let gray_light_dusty = UIColor(hex: 0xE9E9E9)
-    
-    // Create a UIColor from RGB
-    convenience init(red: Int, green: Int, blue: Int, a: CGFloat = 1.0) {
-        self.init(
-            red: CGFloat(red) / 255.0,
-            green: CGFloat(green) / 255.0,
-            blue: CGFloat(blue) / 255.0,
-            alpha: a
-        )
-    }
-    
-    // Create a UIColor from a hex value (E.g 0x000000)
-    convenience init(hex: Int, a: CGFloat = 1.0) {
-        self.init(
-            red: (hex >> 16) & 0xFF,
-            green: (hex >> 8) & 0xFF,
-            blue: hex & 0xFF,
-            a: a
-        )
-    }
-    
-    
-    func createImageFromUIColor() -> UIImage {
+    /// Create a UIImage with a size of 1x1 with the color
+    /// - Returns: the UIImage with a color applied an size 1x1
+    public func createImageFromUIColor() -> UIImage {
         let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
         UIGraphicsBeginImageContext(rect.size)
         let contextRef: CGContext = UIGraphicsGetCurrentContext()!
