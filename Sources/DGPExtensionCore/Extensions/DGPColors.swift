ae6b9fe2adc5af186c1c2ea2a9848a5a897ba6a4
Daniel <1asir.daniel.gallego@gmail.com>
2020-09-28 12:10:18 +0200

    Add some common colors

--- Sources/DGPExtensionCore/Extensions/DGPColors.swift
+++ Sources/DGPExtensionCore/Extensions/DGPColors.swift
@@ -11,6 +11,31 @@ import UIKit
 
 extension UIColor {
     
+    //MAR: - Common colors
+    public static let gray_light_dusty = UIColor(hex: 0xE9E9E9)
+    public static let black_midnight_light = UIColor(hex: 0x404040)
+    public static let transparentBlack = UIColor(hex: 0x000000, a: 0.5)
+    
+    // Create a UIColor from RGB
+    internal convenience init(red: Int, green: Int, blue: Int, a: CGFloat = 1.0) {
+        self.init(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: a
+        )
+    }
+    
+    // Create a UIColor from a hex value (E.g 0x000000)
+    internal convenience init(hex: Int, a: CGFloat = 1.0) {
+        self.init(
+            red: (hex >> 16) & 0xFF,
+            green: (hex >> 8) & 0xFF,
+            blue: hex & 0xFF,
+            a: a
+        )
+    }
+    
     /// Create a UIImage with a size of 1x1 with the color
     /// - Returns: the UIImage with a color applied an size 1x1
     public func createImageFromUIColor() -> UIImage {
